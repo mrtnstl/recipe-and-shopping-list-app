aa6c6a225c673c414370e93afdb33c0c0782a63c
Márton Spiteller <spitellermarton@gmail.com>
2025-09-29 17:31:18 +0200

    add package(-lock).json, change gen user handle funcs postfix from js random to node:crypto randomBytes

--- api/utils/userHelpers.js
+++ api/utils/userHelpers.js
@@ -1,11 +1,11 @@
 import crypto from "node:crypto";
 
-function generateUserId() { // TODO: func not needed, postgres generates uuids
+function generateUserId() { // TODO: func may not be needed, postgres generates uuids
     return crypto.randomUUID();
 }
 function generateUserHandle(userName) {
     const base = userName.toLowerCase();
-    const postfix = (Math.random() * 999999).toFixed();
+    const postfix = crypto.randomBytes(4).toString("hex");
     return `${base}${postfix}`;
 }
 const userHelpers = { generateUserId, generateUserHandle };
